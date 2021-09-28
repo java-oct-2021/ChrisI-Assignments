0b086c38f87103e0c1b51e36fd6902752d47544f
cigot <c.bensi.igot@gmail.com>
2021-09-27 22:20:23 -0400

    first java program

---
+++ 01-java-fundamentals/01-Java First Program/FirstJavaProgram.java
@@ -0,0 +1,8 @@
+public class FirstJavaProgram {
+
+    public static void main(String[] args) {
+        System.out.println("My name is Chris");
+        System.out.println("I am 3700 years old");
+        System.out.println("My hometown is somewhere over there");
+    }
+}
