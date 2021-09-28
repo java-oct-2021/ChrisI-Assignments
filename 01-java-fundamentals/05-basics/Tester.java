25b68ef7dd85839c163b8767f66ed0d48de579c6
cigot <c.bensi.igot@gmail.com>
2021-09-28 05:07:49 -0400

    tester added

---
+++ 01-java-fundamentals/05-basics/Tester.java
@@ -0,0 +1,10 @@
+public class Tester {
+    public static void main(String[] args) {
+        BasicJava.print1to255();
+        System.out.println(BasicJava.sum1to255());
+        BasicJava.printArray();
+        int[] arr = {5,2,6,8,1,4,4,9};
+        BasicJava.findMax(arr);
+        BasicJava.odds1to255();
+    }
+}
