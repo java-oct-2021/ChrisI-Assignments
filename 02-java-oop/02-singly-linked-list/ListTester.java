e0c71e51818940446da5d3f1444321526416bdb5
Christiani Igot <c.bensi.igot@gmail.com>
2021-10-03 04:25:42 -0400

    Singly Linked and ready to have time sinked: 2

--- 02-java-oop/02-singly-linked-list/ListTester.java
+++ 02-java-oop/02-singly-linked-list/ListTester.java
@@ -9,12 +9,30 @@ public class ListTester {
 		sll.add(2);
 		sll.printValues();
 		sll.remove();
-		sll.remove();
-		sll.remove();
-		sll.remove();
-		sll.remove();
+		Node temp;
+		Integer result = null;
+		temp = sll.find(3);
+		if(temp != null){
+			result = temp.value;
+		} else {
+			result = null;
+		}
+		System.out.println("find result:"+result);
+		sll.removeAt(2);
 		sll.printValues();
+		temp = sll.find(5);
+		if(temp != null){
+			result = temp.value;
+		} else {
+			result = null;
+		}
+		System.out.println("find result:"+result);
+		sll.removeAt(4);
+		sll.removeAt(-1);
+		sll.removeAt(0);
+		sll.removeAt(2);
 		sll.remove();
+		sll.removeAt(0);
 		sll.printValues();
 	}
 }
--- 02-java-oop/02-singly-linked-list/SinglyLinkedList.java
+++ 02-java-oop/02-singly-linked-list/SinglyLinkedList.java
@@ -1,38 +1,101 @@
+import java.util.function.BiFunction;
 public class SinglyLinkedList {
 	public Node head;
 	public SinglyLinkedList() {
 
 	}
-	private Node run() {
+	private <T> Node r(BiFunction<Node,T,Boolean> tester, T val) {
 		if(this.head == null) {
 			return this.head;
 		} else {
 			Node runner = this.head;
-			while(runner.next != null) {
-					runner = runner.next;
+			while(!tester.apply(runner, val)) {
+				runner = runner.next;
 			}
 			return runner;
 		}
 	}
-	private Node runPenultimate() {
-		if(this.head == null) {
-			return this.head;
+	private static <T> Boolean isEnd(Node node, T val) {
+		return node.next == null;
+	}
+	private static <T> Boolean printAll(Node node, T val) {
+		System.out.println(node.value);
+		return isEnd(node, val);
+	}
+	private static <T> Boolean isPenultimate(Node node,T val) {
+		if(isEnd(node, val)){	//Required for when the only node is the head
+			return true;
+		} else if (node.next.next == null) {
+			return true;
 		} else {
-			Node runner = this.head;
-			while(runner.next != null) {
-				Node temp = runner.next;
-				if(temp.next == null) {
-					break;
-				} else {
-					runner = runner.next;
-				}
-			}
-			return runner;
+			return false;
 		}
 	}
+	private static <T> Boolean matchVal(Node node, T val) {
+		if(isEnd(node, val)) {
+			return true;
+		} else {
+			int value = (int) val;
+			return value == node.value;
+		}
+	}
+	private static <T> Boolean isPreviousNode(Node node, T val) {
+		if(isEnd(node, val)){
+			return true;
+		} else if(node.next == (Node) val) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	private Node tail() {
+		return r(SinglyLinkedList::isEnd, 0);
+	}
+	private Node preTail() {
+		return r(SinglyLinkedList::isPenultimate,0);
+	}
+	public Node find(int value) {
+		Node foundNode = r(SinglyLinkedList::matchVal,value);
+		if (foundNode.value == value) {	//This checks if the value at the end matches due to how the runner works
+			return foundNode;
+		} else {
+			return null;
+		}
+	}
+	
+	public void removeAt(int index) {
+		Node runner = this.head;
+		if (runner == null) {
+			System.out.println("empty list");
+			return;
+		}
+		if (index < 0) {
+			System.out.println("invalid index (index < 0)");
+			return;
+		}
+		for (int i = 0; i < index; i++) {
+			if (index == i) {
+				break;
+			}
+			if(runner.next == null) {
+				System.out.println("invalid index (index > list length)");
+				return;
+			}
+			runner = runner.next;
+		}
+
+		if(runner == this.head) {
+			this.head = this.head.next;
+			return;
+		} else {
+			Node prev = r(SinglyLinkedList::isPreviousNode, runner);
+			prev.next = runner.next;
+		}
+	}
+	
 	public void add(int value) {
 		Node newNode = new Node(value);
-		Node runner = run();
+		Node runner = tail();
 		if(runner == null) {
 			this.head = newNode;
 		} else {
@@ -45,21 +108,12 @@ public class SinglyLinkedList {
 		} else if (this.head.next == null) {
 			this.head = null;
 		} else {
-			runPenultimate().next = null;
+			preTail().next = null;
 		}
 	}
 	public void printValues() {
-		System.out.println("\n");
-		if(this.head == null) {
-			return;
-		} else {
-			Node runner = this.head;
-			System.out.println(runner.value);
-			
-			while(runner.next != null) {
-				runner = runner.next;
-				System.out.println(runner.value);
-			}
-		}
+		System.out.println("----print values----");
+		r(SinglyLinkedList::printAll,0);
+		System.out.println("---------end--------");
 	}
 }
